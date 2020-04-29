9959799275eaa92e27754a5fcde791d3ca667781
Marcio R Sanches <marciosanches@outlook.com>
2020-04-29 19:53:50 -0300

    exercicio 053

---
+++ Desafio053.py
@@ -0,0 +1,13 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+
+palavras = frase.split()
+juntar = ''.join(palavras)
+inverso = ''
+for letra in range(len(juntar) -1, -1 -1):
+    inverso = inverso + juntar[letra]
+print('O inverso de {} é {}'.format(juntar, inverso))
+if inverso == juntar:
+    print('Temos um PALÍNDROMO!')
+else:
+    print('Não temos um PALÍNDROMO!')
+
