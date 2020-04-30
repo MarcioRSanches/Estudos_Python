2888cb92cf472a7dddd5b2cadbf6331c5f92a2ef
Marcio R Sanches <marciosanches@outlook.com>
2020-04-30 18:14:44 -0300

    correção exercicio 53

--- Desafio053.py
+++ Desafio053.py
@@ -3,7 +3,7 @@ frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 juntar = ''.join(palavras)
 inverso = ''
-for letra in range(len(juntar) -1, -1 -1):
+for letra in range(len(juntar) -1, -1, -1):
     inverso = inverso + juntar[letra]
 print('O inverso de {} é {}'.format(juntar, inverso))
 if inverso == juntar:
