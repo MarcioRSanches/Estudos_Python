f9419e58ff93dd2375d822794c001284b357c552
Marcio R Sanches <marciosanches@outlook.com>
2020-05-07 20:20:31 -0300

    tuplas - exercicio 72

---
+++ Tuplas_Desafio072.py
@@ -0,0 +1,8 @@
+extenso = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+
+while True:
+    numero = int(input('Digite um número [de: 0 a 20]: '))
+    if 0 <= numero <= 20:
+        break
+    print('Tente novamente. ', end='' )
+print(f'O número digitado foi {extenso[numero]}')
