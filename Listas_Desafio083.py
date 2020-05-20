d8ff7ffffad6f9e8397608cb05dd7c40b5c35b34
Marcio R Sanches <marciosanches@outlook.com>
2020-05-20 18:39:14 -0300

    lista exercicio 83

---
+++ Listas_Desafio083.py
@@ -0,0 +1,16 @@
+expressao = str(input('Digite sua expressão matemática: '))
+pilha = []
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('A expressão está CORRETA!')
+else:
+    print('A expressão está INCORRETA!')
+
