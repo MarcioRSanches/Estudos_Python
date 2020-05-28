7943364d74e459f94ca4004b977f0b508f1a4e5d
Marcio R Sanches <marciosanches@outlook.com>
2020-05-27 21:30:21 -0300

    ajustes

--- Dicionario_Desafio093.py
+++ Dicionario_Desafio093.py
@@ -4,7 +4,7 @@ jogador['nome'] = str(input('Jogador: '))
 total = int(input(f'Quantas partidas {jogador["nome"]}: '))
 if total != 0:
     for c in range(0, total):
-        partidas.append(int(input(f'- Quantos gols na partida {c} ?: ')))
+        partidas.append(int(input(f'- Quantos gols na partida {c+1} ?: ')))
 jogador['gols'] = partidas[:]
 jogador['total'] = sum(partidas)
 partidas.clear()
@@ -14,5 +14,5 @@ for k, v in jogador.items():
 print('-='*30)
 print(f' O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
 for i, v in enumerate(jogador['gols']):
-    print(f' ** Na partida {i+}, fez {v} gols')
+    print(f' ** Na partida {i+1}, fez {v} gols')
 print(f'Total de gols: {jogador["total"]}')
