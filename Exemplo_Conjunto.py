47f03c237d1f0c8c162a4e9034fc2775762c07ca
Marcio R Sanches <marciosanches@outlook.com>
2020-05-29 08:42:49 -0300

    exemplo conjunto

---
+++ Exemplo_Conjunto.py
@@ -0,0 +1,20 @@
+conjunto = {1,2,3,4,4}
+conjunto2 = {4,5,6,7,8}
+
+conjunto_uniao = conjunto.union(conjunto2)
+conjunto_interseccao = conjunto.intersection(conjunto2)
+conjunto_diferenca = conjunto.difference(conjunto2)
+conjunto_diferenca_simetrica = conjunto.symmetric_difference(conjunto2)
+
+conjunto_a = {1,2,3}
+
+print(conjunto_uniao)
+print(conjunto_interseccao)
+print(conjunto_diferenca)
+print(conjunto_diferenca_simetrica)
+
+#print(conjunto)
+#conjunto.add(5)
+#print(conjunto)
+#conjunto.discard(2)
+#print(conjunto)
