62ffb87d00708b407607a84be2cf952f4d8a536d
Marcio R Sanches <marciosanches@outlook.com>
2020-06-03 19:02:03 -0300

    Executando exercio 97 - Funcoes

---
+++ Funcoes_Desafio097.py
@@ -0,0 +1,10 @@
+def escreva(mensagem):
+    tamanho = len(mensagem) + 4
+    print('=' * tamanho)
+    print(f'  {mensagem}')
+    print('=' * tamanho)
+
+
+escreva('Estundando Python')
+escreva('Título das tarefas criadas nos exercícios')
+escreva('Brasil')
