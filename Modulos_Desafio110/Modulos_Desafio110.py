e31b1f815b25db6f8bc3b403d68dabe57aba2367
Marcio R Sanches <marciosanches@outlook.com>
2020-06-07 15:23:03 -0300

    Modulos Exercicio 110

---
+++ Modulos_Desafio110/Modulos_Desafio110.py
@@ -0,0 +1,8 @@
+import moeda
+
+valor = float(input('Digite o preço da mercadoria: R$ ' ))
+taxaa = float(input('Taxa de aumento: '))
+taxar = float(input('Taxa de redução: '))
+
+moeda.resumo(valor,taxaa,taxar)
+#moeda.resumo(valor,taxa)
