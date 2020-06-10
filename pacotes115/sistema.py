3c0c25e49f850b6187b29caae011edb7bc3e06d5
Marcio R Sanches <marciosanches@outlook.com>
2020-06-09 22:05:49 -0300

    try e manipulação de arquivo txt

--- pacotes115/sistema.py
+++ pacotes115/sistema.py
@@ -1,14 +1,25 @@
+from pacotes115.lib.arquivo import *
 from pacotes115.lib.interface import *
+
+
+arq = 'listapessoas.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opção 1')
+        lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabecalho('Saindo do sistema... Até logo!!')
+        break
     else:
         cabecalho('\033[31mErro! Digite uma opção valida!\033[m')
 
